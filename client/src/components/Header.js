3ebd8708475740fb99ec8f32a5ed84482eea0762
Gabriel Salkin <salkinator@gmail.com>
2019-05-08 16:40:11 -0400

    Update static routes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,15 +5,17 @@ class Header extends React.Component {
 		return (
 			<nav className="navbar navbar-light">
 				<a className="navbar-brand" href="/view/all">
-					<h2>MultiBeast</h2>
+					<img src="/images/favicon/apple-icon.png" width="50" height="50" alt="" />
+					&nbsp;
+					MultiBeast
 				</a>
 				<div className="form-inline">
 					<h3>
-						<a href="/view/all/SH%202018" className={'btn purple__aspen ' + this.props.status}>
+						<a href="/view/season/SH%202018" className={'btn purple__aspen ' + this.props.status}>
 							Aspen Ideas Health
 						</a>
 						&nbsp;
-						<a href="/view/all/AIF%202018" className={'btn brightblue__aspen ' + this.props.status}>
+						<a href="/view/season/AIF%202018" className={'btn brightblue__aspen ' + this.props.status}>
 							Aspen Ideas Festival
 						</a>
 					</h3>
--- client/src/components/SessionListItem.js
+++ client/src/components/SessionListItem.js
@@ -4,85 +4,114 @@ import * as Helpers from '../helpers';
 class SessionListItem extends React.Component {
 	render() {
 		const {
-			ArtsVisionFork: { SessionName, SessionDate, SessionLocation, StartTime, EndTime, EventID, SessionSpeakers, SessionFest },
+			ArtsVisionFork: {
+				SessionName,
+				SessionDate,
+				SessionLocation,
+				StartTime,
+				EndTime,
+				EventID,
+				SessionSpeakers,
+				SessionFest
+			},
 			AspenChecklistFork: { Status },
 			AspenCoverageFork: { VideoVenue, VideoRover, LiveStream, QuickClip, Audio, Photo, Transcript, Restriction }
 		} = this.props.data;
-		
+
 		const seasonClass = function() {
-			if (Helpers.seasonMarker(SessionFest) == 'Aspen Ideas Health') {
-				return 'purple__aspen'
+			if (Helpers.seasonMarker(SessionFest) === 'Aspen Ideas Health') {
+				return 'purple__aspen';
 			} else {
-				return 'brightblue__aspen'
+				return 'brightblue__aspen';
 			}
-		}
+		};
 		return (
-			<div
-				className="list-group-item list-group-item-action"
-				id={'event-' + EventID}
-				data-session-label={SessionName}
-			>
-				<div className="d-flex w-100 justify-content-between">
-					<h4 className="mb-1">
-						<small className="text-muted">
-							<a href={'/view/all/' + encodeURIComponent(SessionFest)} className={'badge ' + seasonClass() }>{Helpers.seasonMarker(SessionFest)}</a>
-							&nbsp;
-							<a className={Helpers.dateClassHelper(SessionDate)} href={'/view/date/' + SessionDate}>
-								{SessionDate}
-							</a>
-							&nbsp;
-							<span className="badge border border-primary rounded-lg">
-								{Helpers.convertTimes(StartTime)} to {Helpers.convertTimes(EndTime)}
-							</span>
-						</small>
-						<br />
-						<a href={'/session/' + EventID} className="text-dark">
-							{SessionName}
-						</a>
-					</h4>
-					<h5>
-						<strong>#{EventID}</strong>
-						<br />
-						<span className={'badge ' + Helpers.classHelper(Status)}>{Status}</span>
-					</h5>
+			<div className="card" id={'event-' + EventID} data-session-label={SessionName}>
+				<div className="card-header">
+					<a href={'/view/season/' + encodeURIComponent(SessionFest)} className={'badge ' + seasonClass()}>
+						{Helpers.seasonMarker(SessionFest)}
+					</a>
+					&nbsp;
+					<a className={Helpers.dateClassHelper(SessionDate)} href={'/view/date/' + SessionDate}>
+						{SessionDate}
+					</a>
+					&nbsp;
+					<span className="badge border border-primary rounded-lg">
+						{Helpers.convertTimes(StartTime)} to {Helpers.convertTimes(EndTime)}
+					</span>
 				</div>
-				<a href={'/view/location/' + encodeURIComponent(SessionLocation)} className="mb-1">
-					{SessionLocation}
-				</a>
-				<br />
-				<small>{Helpers.stringifySpeakers(SessionSpeakers)}</small>
-				<hr />
-				{VideoVenue && (
-					<a href={'/view/all/VideoVenue'} className="badge badge-pill badge-dark">Recording ✓</a>
-				)}
-				&nbsp;
-				{VideoRover && (
-					<a href={'/view/all/VideoRover'} className="badge badge-pill badge-dark">Send Rover ✓</a>
-				)}
-				&nbsp;
-				{LiveStream && (
-					<a href={'/view/all/LiveStream'} className="badge badge-pill badge-dark">Livestream ✓</a>
-				)}
-				&nbsp;
-				{QuickClip && (
-					<a href={'/view/all/QuickClip'} className="badge badge-pill badge-dark">QuickClips ✓</a>
-				)}
-				&nbsp;
-				{Photo && (
-					<a href={'/view/all/Photo'} className="badge badge-pill badge-dark">Photo Priority ✓</a>
-				)}
-				&nbsp;
-				{Audio && (
-					<a href={'/view/all/Audio'} className="badge badge-pill badge-dark">Audio Priority ✓</a>
-				)}
-				&nbsp;
-				{Transcript && (
-					<a href={'/view/all/Transcript'} className="badge badge-pill badge-dark">Transcript Priority ✓</a>
-				)}
-				&nbsp;
-				{Restriction && (
-					<a href={'/view/all/Restriction'} className="badge badge-pill badge-danger">Restriction X</a>
-				)}
+				<div className="card-body">
+					<div className="row">
+						<div className="col-10">
+							<h4>
+								<a href={'/session/' + EventID} className="text-dark">
+									{SessionName}
+								</a>
+							</h4>
+						</div>
+						<div className="col-2">
+							<h5 className="text-right">
+								<strong className="align-middle">#{EventID}</strong>
+								&nbsp;
+								<span className={'h5 badge ' + Helpers.classHelper(Status)}>{Status}</span>
+							</h5>
+						</div>
+					</div>
+					<a href={'/view/location/' + encodeURIComponent(SessionLocation)} className="mb-1">
+						{SessionLocation}
+					</a>
+					<br />
+					<small>{Helpers.stringifySpeakers(SessionSpeakers)}</small>
+				</div>
+				<div className="card-footer">
+					{VideoVenue && (
+						<a href={'/view/type/VideoVenue'} className="badge badge-pill badge-dark">
+							Recording ✓
+						</a>
+					)}
+					&nbsp;
+					{VideoRover && (
+						<a href={'/view/type/VideoRover'} className="badge badge-pill badge-dark">
+							Send Rover ✓
+						</a>
+					)}
+					&nbsp;
+					{LiveStream && (
+						<a href={'/view/type/LiveStream'} className="badge badge-pill badge-dark">
+							Livestream ✓
+						</a>
+					)}
+					&nbsp;
+					{QuickClip && (
+						<a href={'/view/type/QuickClip'} className="badge badge-pill badge-dark">
+							QuickClips ✓
+						</a>
+					)}
+					&nbsp;
+					{Photo && (
+						<a href={'/view/type/Photo'} className="badge badge-pill badge-dark">
+							Photo Priority ✓
+						</a>
+					)}
+					&nbsp;
+					{Audio && (
+						<a href={'/view/type/Audio'} className="badge badge-pill badge-dark">
+							Audio Priority ✓
+						</a>
+					)}
+					&nbsp;
+					{Transcript && (
+						<a href={'/view/all/Transcript'} className="badge badge-pill badge-dark">
+							Transcript Priority ✓
+						</a>
+					)}
+					&nbsp;
+					{Restriction && (
+						<a href={'/view/all/Restriction'} className="badge badge-pill badge-danger">
+							Restriction X
+						</a>
+					)}
+				</div>
 			</div>
 		);
 	}
