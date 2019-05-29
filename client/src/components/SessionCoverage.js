a862a92894c87d0915365b9a3816e532cfbc3f35
Gabriel Salkin <salkinator@gmail.com>
2019-05-29 15:50:13 -0400

    Update Session Pages with Collapses

--- client/src/components/SessionCoverage.js
+++ client/src/components/SessionCoverage.js
@@ -24,12 +24,16 @@ class SessionCoverage extends React.Component {
 				transcript: this.transcriptRef.current.checked,
 				audio: this.audioRef.current.checked,
 				restriction: this.restrictionRef.current.checked,
-				notes: this.coverageNotesRef.current.value.length > 1 ? 
-					this.props.details.AspenNotes +
-					'\n\n' +
-					this.props.user + ' on ' + format(Date.now(), 'YYYY-MM-DD@h:mmA') +
-					': ' +
-					this.coverageNotesRef.current.value : this.props.details.AspenNotes
+				notes:
+					this.coverageNotesRef.current.value.length > 1
+						? this.props.details.AspenNotes +
+						  '\n\n' +
+						  this.props.user +
+						  ' on ' +
+						  format(Date.now(), 'YYYY-MM-DD@h:mmA') +
+						  ': ' +
+						  this.coverageNotesRef.current.value
+						: this.props.details.AspenNotes
 			}
 		};
 		this.props.updateSession(body);
@@ -38,170 +42,138 @@ class SessionCoverage extends React.Component {
 
 	render() {
 		return (
-			<form id="coveragePlanForm" onSubmit={this.createCoverage}>
-				<div className="form-row">
-					<div className="form-group">
-						<div className="form-check form-check-inline">
-							<input
-								className="form-check-input"
-								type="checkbox"
-								name="video"
-								ref={this.videoRef}
-								id="inputVideo"
-								defaultChecked={this.props.details.VideoVenue}
-							/>
-							<label htmlFor="inputVideo" className="form-check-label col-form-label col-form-label-lg">
-								Record Video
-							</label>
-						</div>
-					</div>
-					<div className="form-group">
-						<div className="form-check form-check-inline">
-							<input
-								className="form-check-input"
-								type="checkbox"
-								name="rover"
-								ref={this.roverRef}
-								id="inputRover"
-								defaultChecked={this.props.details.VideoRover}
-							/>
-							<label htmlFor="inputRover" className="form-check-label col-form-label col-form-label-lg">
-								Send Rover
-							</label>
-						</div>
-					</div>
-					<div className="form-group">
-						<div className="form-check form-check-inline">
-							<input
-								type="checkbox"
-								className="form-check-input"
-								id="inputLivestream"
-								ref={this.livestreamRef}
-								name="livestream"
-								defaultChecked={this.props.details.LiveStream}
-							/>
-							<label
-								htmlFor="inputLivestream"
-								className="form-check-label col-form-label col-form-label-lg"
-							>
-								Livestream
-							</label>
-						</div>
-					</div>
-					<div className="form-group">
-						<div className="form-check form-check-inline">
-							<input
-								type="checkbox"
-								className="form-check-input"
-								id="inputQuickclip"
-								ref={this.quickclipRef}
-								name="quickclip"
-								defaultChecked={this.props.details.QuickClip}
-							/>
-							<label
-								htmlFor="inputQuickclip"
-								className="form-check-label col-form-label col-form-label-lg"
-							>
-								Quick Clip
-							</label>
-						</div>
-					</div>
-					<div className="form-group">
-						<div className="form-check form-check-inline">
-							<input
-								type="checkbox"
-								className="form-check-input"
-								id="inputPhoto"
-								ref={this.photoRef}
-								name="photo"
-								defaultChecked={this.props.details.Photo}
-							/>
-							<label htmlFor="inputPhoto" className="form-check-label col-form-label col-form-label-lg">
-								Photo Priority
-							</label>
-						</div>
-					</div>
-					<div className="form-group">
-						<div className="form-check form-check-inline">
-							<input
-								type="checkbox"
-								className="form-check-input"
-								id="inputTranscript"
-								ref={this.transcriptRef}
-								name="transcript"
-								defaultChecked={this.props.details.Transcript}
-							/>
-							<label
-								htmlFor="inputTranscript"
-								className="form-check-label col-form-label col-form-label-lg"
-							>
-								Rush Transcript
-							</label>
-						</div>
-					</div>
-					<div className="form-group">
-						<div className="form-check form-check-inline">
-							<input
-								type="checkbox"
-								className="form-check-input"
-								id="inputAudio"
-								ref={this.audioRef}
-								name="audio"
-								defaultChecked={this.props.details.Audio}
-							/>
-							<label htmlFor="inputPodcast" className="form-check-label col-form-label col-form-label-lg">
-								Audio Priority
-							</label>
-						</div>
-					</div>
-					<div className="form-group">
-						<div className="form-check form-check-inline">
-							<input
-								type="checkbox"
-								className="form-check-input"
-								id="inputrestriction"
-								ref={this.restrictionRef}
-								name="restriction"
-								defaultChecked={this.props.details.Restriction}
-							/>
-							<label htmlFor="inputPodcast" className="form-check-label col-form-label col-form-label-lg">
-								Restriction
-							</label>
-						</div>
-					</div>
-				</div>
-				<div className="bg-light">
-					<pre>
-						<p>{this.props.details.AspenNotes}</p>
-					</pre>
-				</div>
-
-				<div className="form-row">
-					<div className="form-group col-8 offset-2">
-						<label htmlFor="inputCoverageNotes" className="col-form-label col-form-label-lg">
-							Notes <small>(Please note if changing the above information)</small>:
-						</label>
-						<textarea
-							className="form-control"
-							name="coveragenotes"
-							id="inputCoverageNotes"
-							ref={this.coverageNotesRef}
-							rows="5"
-							defaultValue={this.props.details.AspenNotes}
-							//onChange={this.handleInputChange.bind(this)}
+			<form id="coveragePlanForm" onSubmit={this.createCoverage} className="row collapse">
+				<div className="form-group col-3 px-5">
+					<div className="form-check">
+						<input
+							className="form-check-input"
+							type="checkbox"
+							name="video"
+							ref={this.videoRef}
+							id="inputVideo"
+							defaultChecked={this.props.details.VideoVenue}
 						/>
+						<label htmlFor="inputVideo" className="form-check-label col-form-label col-form-label-md">
+							Record Video
+						</label>
 					</div>
+
+					<div className="form-check">
+						<input
+							className="form-check-input"
+							type="checkbox"
+							name="rover"
+							ref={this.roverRef}
+							id="inputRover"
+							defaultChecked={this.props.details.VideoRover}
+						/>
+						<label htmlFor="inputRover" className="form-check-label col-form-label col-form-label-md">
+							Send Rover
+						</label>
+					</div>
+					<div className="form-check">
+						<input
+							type="checkbox"
+							className="form-check-input"
+							id="inputLivestream"
+							ref={this.livestreamRef}
+							name="livestream"
+							defaultChecked={this.props.details.LiveStream}
+						/>
+						<label htmlFor="inputLivestream" className="form-check-label col-form-label col-form-label-md">
+							Livestream
+						</label>
+					</div>
+					<div className="form-check">
+						<input
+							type="checkbox"
+							className="form-check-input"
+							id="inputQuickclip"
+							ref={this.quickclipRef}
+							name="quickclip"
+							defaultChecked={this.props.details.QuickClip}
+						/>
+						<label htmlFor="inputQuickclip" className="form-check-label col-form-label col-form-label-md">
+							Quick Clip
+						</label>
+					</div>
+					<div className="form-check">
+						<input
+							type="checkbox"
+							className="form-check-input"
+							id="inputPhoto"
+							ref={this.photoRef}
+							name="photo"
+							defaultChecked={this.props.details.Photo}
+						/>
+						<label htmlFor="inputPhoto" className="form-check-label col-form-label col-form-label-md">
+							Photo Priority
+						</label>
+					</div>
+					<div className="form-check">
+						<input
+							type="checkbox"
+							className="form-check-input"
+							id="inputTranscript"
+							ref={this.transcriptRef}
+							name="transcript"
+							defaultChecked={this.props.details.Transcript}
+						/>
+						<label htmlFor="inputTranscript" className="form-check-label col-form-label col-form-label-md">
+							Rush Transcript
+						</label>
+					</div>
+					<div className="form-check">
+						<input
+							type="checkbox"
+							className="form-check-input"
+							id="inputAudio"
+							ref={this.audioRef}
+							name="audio"
+							defaultChecked={this.props.details.Audio}
+						/>
+						<label htmlFor="inputPodcast" className="form-check-label col-form-label col-form-label-md">
+							Audio Priority
+						</label>
+					</div>
+					<div className="form-check">
+						<input
+							type="checkbox"
+							className="form-check-input"
+							id="inputrestriction"
+							ref={this.restrictionRef}
+							name="restriction"
+							defaultChecked={this.props.details.Restriction}
+						/>
+						<label htmlFor="inputPodcast" className="form-check-label col-form-label col-form-label-md">
+							Restriction
+						</label>
+					</div>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<button type="submit" form="coveragePlanForm" className="btn btn-primary">
+						Save Coverage
+					</button>
 				</div>
-				<div className="form-row">
-					<div className="form-group col-3 offset-2">
-						<button type="submit" form="coveragePlanForm" className="btn btn-primary">
-							Save Coverage
-						</button>
-						&nbsp;
-						<button type="submit" form="coveragePlanForm" className="btn btn-dark">
-							Save &amp; Exit
-						</button>
+				<div className="form-group col-9">
+					<div className="bg-light">
+						<pre>
+							<small>{this.props.details.AspenNotes}</small>
+						</pre>
 					</div>
+					<label htmlFor="inputCoverageNotes" className="">
+						<small>Enter Notes Here:</small>
+					</label>
+					<textarea
+						className="form-control"
+						name="coveragenotes"
+						id="inputCoverageNotes"
+						ref={this.coverageNotesRef}
+						rows="5"
+						defaultValue={this.props.details.AspenNotes}
+					/>
 				</div>
 			</form>
 		);
--- client/src/components/SessionWorkflow.js
+++ client/src/components/SessionWorkflow.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import QuickClip from './QuickClip';
 
 class SessionWorkflow extends React.Component {
@@ -57,7 +57,7 @@ class SessionWorkflow extends React.Component {
 
 	saveQuickClip = event => {
 		event.preventDefault();
-		
+
 		const qcInstance = {
 			timecodeIn: this.timecodeInRef.current.value,
 			timecodeOut: this.timecodeOutRef.current.value,
@@ -65,7 +65,7 @@ class SessionWorkflow extends React.Component {
 			outro: this.outroRef.current.value,
 			caption: this.captionRef.current.value
 		};
-		this.setState({qc: [...this.state.qc, qcInstance]})
+		this.setState({ qc: [...this.state.qc, qcInstance] });
 		event.currentTarget.reset();
 	};
 
@@ -74,204 +74,127 @@ class SessionWorkflow extends React.Component {
 		console.log('3: ' + this.state.qc);
 		let index = event.target.parentNode.id;
 		let array = this.state.qc;
-		array.splice(index, 1)
+		array.splice(index, 1);
 		this.setState({
 			qc: array
-		})
-	}
+		});
+	};
 
 	render() {
 		return (
-			<div className="container">
-				<div className="form-row">
-					<div className="col">
-						<h4>Workflow</h4>
-						<form id="workflowForm" onSubmit={this.updateWorkflow}>
-							<div className="form-check form-check-inline">
-								<input
-									className="form-check-input"
-									type="checkbox"
-									name="recorded"
-									ref={this.recordedRef}
-									id="inputRecorded"
-									defaultChecked={this.props.details.Recorded}
-									value="Recorded"
-								/>
-								<label htmlFor="inputRecorded" className="form-check-label">
-									Recorded
-								</label>
-							</div>
-							<div className="form-check form-check-inline">
-								<input
-									className="form-check-input"
-									type="checkbox"
-									name="rendered"
-									id="inputRendered"
-									ref={this.renderedRef}
-									defaultChecked={this.props.details.Rendered}
-									value="Rendered"
-								/>
-								<label htmlFor="inputRendered" className="form-check-label">
-									Rendered
-								</label>
-							</div>
-							<div className="form-check form-check-inline">
-								<input
-									className="form-check-input"
-									type="checkbox"
-									name="quickcliprendered"
-									id="inputQuickClipRendered"
-									ref={this.quickclipRenderedRef}
-									defaultChecked={this.props.details.QuickClipRendered}
-									
-								/>
-								<label htmlFor="inputQuickClipRendered" className="form-check-label">
-									QuickClips Rendered
-								</label>
-							</div>
-							<div className="form-check form-check-inline">
-								<input
-									className="form-check-input"
-									type="checkbox"
-									name="complete"
-									id="inputComplete"
-									ref={this.completeRef}
-									defaultChecked={this.props.details.Complete}
-								/>
-								<label htmlFor="inputComplete" className="form-check-label">
-									Complete
-								</label>
-							</div>
-							<label htmlFor="youtubeURLInput">Youtube URL</label>
+			<div className="container-fluid collapse" id="workflowContainer">
+				<form id="workflowForm" onSubmit={this.updateWorkflow} className="row">
+					<div className="form-group col-3 px-2">
+						<div className="form-check">
 							<input
-								type="text"
-								className="form-control"
-								placeholder={this.props.details.YouTubeURL}
-								ref={this.youtubeURLRef}
-								id="youtubeURLInput"
+								className="form-check-input"
+								type="checkbox"
+								name="recorded"
+								ref={this.recordedRef}
+								id="inputRecorded"
+								defaultChecked={this.props.details.Recorded}
+								value="Recorded"
 							/>
-							<label htmlFor="sessionURLInput">Session URL</label>
+							<label htmlFor="inputRecorded" className="form-check-label col-form-label col-form-label-md">
+								Recorded
+							</label>
+						</div>
+						<div className="form-check">
 							<input
-								type="text"
-								className="form-control"
-								placeholder={this.props.details.SessionURL}
-								ref={this.sessionURLRef}
-								id="sessionURLInput"
+								className="form-check-input"
+								type="checkbox"
+								name="rendered"
+								id="inputRendered"
+								ref={this.renderedRef}
+								defaultChecked={this.props.details.Rendered}
+								value="Rendered"
 							/>
-							<label htmlFor="albumURLInput">Album URL</label>
+							<label htmlFor="inputRendered" className="form-check-label col-form-label col-form-label-md">
+								Rendered
+							</label>
+						</div>
+						<div className="form-check">
 							<input
-								type="text"
-								className="form-control"
-								placeholder={this.props.details.AlbumURL}
-								ref={this.albumURLRef}
-								id="albumURLInput"
+								className="form-check-input"
+								type="checkbox"
+								name="quickcliprendered"
+								id="inputQuickClipRendered"
+								ref={this.quickclipRenderedRef}
+								defaultChecked={this.props.details.QuickClipRendered}
 							/>
-							<label htmlFor="audioURLInput">Audio URL</label>
+							<label htmlFor="inputQuickClipRendered" className="form-check-label col-form-label col-form-label-md">
+								QuickClips Rendered
+							</label>
+						</div>
+						<div className="form-check">
 							<input
-								type="text"
-								className="form-control"
-								placeholder={this.props.details.AudioURL}
-								ref={this.audioURLRef}
-								id="audioURLInput"
+								className="form-check-input"
+								type="checkbox"
+								name="complete"
+								id="inputComplete"
+								ref={this.completeRef}
+								defaultChecked={this.props.details.Complete}
 							/>
-							<label htmlFor="transcriptURLInput">Transcript URL</label>
-							<input
-								type="text"
-								className="form-control"
-								placeholder={this.props.details.TranscriptURL}
-								ref={this.transcriptURLRef}
-								id="transcriptURLInput"
-							/>
-						</form>
+							<label htmlFor="inputComplete" className="form-check-label col-form-label col-form-label-md">
+								Complete
+							</label>
+						</div>
+						<button type="submit" form="workflowForm" className="btn btn-primary">
+							Save Coverage
+						</button>
 					</div>
-					<div className="col">
-						<form action="" id="quickclipEntry" onSubmit={this.saveQuickClip}>
-							<h5>QuickClips Entry Form</h5>
-							<div className="p-3 mb-2 bg-warning text-dark">
-								<small>
-									Please save QuickClips before refreshing page.
-								</small>
-							</div>
-							<div className="input-group mb-3">
-								<input
-									type="text"
-									className="form-control"
-									placeholder="Timecode IN"
-									ref={this.timecodeInRef}
-									required
-								/>
-							</div>
-							<div className="input-group mb-3">
-								<input
-									type="text"
-									className="form-control"
-									placeholder="Timecode OUT"
-									ref={this.timecodeOutRef}
-									required
-								/>
-							</div>
-							<div className="input-group mb-3">
-								<input
-									type="text"
-									className="form-control"
-									placeholder="Intro"
-									ref={this.introRef}
-									required
-								/>
-							</div>
-							<div className="input-group mb-3">
-								<input
-									type="text"
-									className="form-control"
-									placeholder="Outro"
-									ref={this.outroRef}
-									required
-								/>
-							</div>
-							<div className="form-group mb-3">
-								<label htmlFor="captionSuggestion">Caption Suggestion</label>
-								<textarea
-									className="form-control"
-									id="captionSuggestion"
-									rows="3"
-									ref={this.captionRef}
-								/>
-							</div>
-							<button type="submit" form="quickclipEntry" className="btn btn-primary">
-								Add QuickClip Entry <br />
-							</button>
-						</form>
-						{this.state.qc.length >= 1 && (
-							<div className="table-responsive">
-								<table className="table">
-									<thead>
-										<tr>
-											<th scope="col">TC In</th>
-											<th scope="col">TC Out</th>
-											<th scope="col">Intro</th>
-											<th scope="col">Outro</th>
-											<th scope="col">Caption</th>
-											<th scope="col"></th>
-										</tr>
-									</thead>
-									<tbody>
-										{this.state.qc.map( (object, index) => (
-											<QuickClip index={index} key={index} data={this.state.qc[index]} deleteQuickClip={this.deleteQuickClip}/>
-										))}
-									</tbody>
-								</table>
-							</div>
-						)}
+					<br />
+					<div className="form-group col-9">
+						<label htmlFor="youtubeURLInput">Youtube URL</label>
+						<input
+							type="text"
+							className="form-control"
+							placeholder={this.props.details.YouTubeURL}
+							ref={this.youtubeURLRef}
+							id="youtubeURLInput"
+						/>
+						<label htmlFor="sessionURLInput">Session URL</label>
+						<input
+							type="text"
+							className="form-control"
+							placeholder={this.props.details.SessionURL}
+							ref={this.sessionURLRef}
+							id="sessionURLInput"
+						/>
+						<label htmlFor="albumURLInput">Album URL</label>
+						<input
+							type="text"
+							className="form-control"
+							placeholder={this.props.details.AlbumURL}
+							ref={this.albumURLRef}
+							id="albumURLInput"
+						/>
+						<label htmlFor="audioURLInput">Audio URL</label>
+						<input
+							type="text"
+							className="form-control"
+							placeholder={this.props.details.AudioURL}
+							ref={this.audioURLRef}
+							id="audioURLInput"
+						/>
+						<label htmlFor="transcriptURLInput">Transcript URL</label>
+						<input
+							type="text"
+							className="form-control"
+							placeholder={this.props.details.TranscriptURL}
+							ref={this.transcriptURLRef}
+							id="transcriptURLInput"
+						/>
 					</div>
-				</div>
+				</form>
 				<br />
 				<button type="submit" form="workflowForm" className="btn btn-primary">
 					Save Coverage
 				</button>{' '}
 				&nbsp;
-				{/* <button type="submit" form="workflowForm" className="btn btn-dark">
+				<button type="submit" form="workflowForm" className="btn btn-dark">
 					Save and Exit
-				</button> */}
+				</button>
 			</div>
 		);
 	}
